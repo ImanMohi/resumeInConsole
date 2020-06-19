6adfd76a756d7493132cd7da29f0132aaf5467e4
Iman Mohi <imanmohi786@gmail.com>
2020-06-19 18:19:52 +0530

    .

---
+++ app.js
@@ -0,0 +1,9 @@
+let intro = {
+    name: "Iman Mohi",
+    Career: "Computer Science Student",
+    Description: "My biggest passions are people and experiences"
+};
+
+console.log("Name: "+ intro.name + "\n");
+console.log("Career: "+ intro.Career + "\n");
+console.log("Description: "+ intro.Description + "\n\n");
